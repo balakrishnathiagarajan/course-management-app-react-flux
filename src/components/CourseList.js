b1edf2b8a6cc70d03d2392792f44d0f01ee9b18f
Balakrishna Thiagarajan <balakrishna.thiagarajan@cerner.com>
2020-05-26 13:45:53 -0500

    Forms

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import ManageCoursePage from "./ManageCoursePage";
 
 function CourseList(props) {
   return (
